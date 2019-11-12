8b1a1f253c96dc11738a20027a652db1319230df
Nathalie Neuhaus <nneuhaus@ucvts.org>
2019-11-12 13:21:55 -0500

    THE STRUGGLE IS REAL

---
+++ src/sum.js
@@ -0,0 +1,8 @@
+const readlineSync = require("readline-sync");
+const MIN = -9007199254740991;
+const MAX = 9007199254740991;
+
+const Lb1 = Number(readlineSync.question("\nLower bound: "));
+const Ub1 = Number(readlineSync.question("\nUpper bound: "));
+const Lb2 = Number(readlineSync.question("\nLower bound: "));
+const Ub2 =
