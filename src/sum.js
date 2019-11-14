506d8118bd49ac3f36bb375b1ac40ef1257dfec2
Nathalie Neuhaus <nneuhaus@ucvts.org>
2019-11-14 13:02:47 -0500

    finally

--- src/sum.js
+++ src/sum.js
@@ -1,8 +1,22 @@
 const readlineSync = require("readline-sync");
 const MIN = -9007199254740991;
 const MAX = 9007199254740991;
-
-const Lb1 = Number(readlineSync.question("\nLower bound: "));
-const Ub1 = Number(readlineSync.question("\nUpper bound: "));
-const Lb2 = Number(readlineSync.question("\nLower bound: "));
-const Ub2 =
+let Low=Number(readlineSync.question("\nLower Bound: "));
+let Up=Number(readlineSync.question("\nUpper Bound: "));
+let Sum=0
+while (Low > Up ) {
+    Low=Number(readlineSync.question("\nLower Bound: "));
+    Up=Number(readlineSync.question("\nUpper Bound: "));
+    }
+if (Low % 2 ===0){
+  for (i=Low; i<=Up;i+=2) {
+    Sum= i + Sum;
+  }
+}
+else if (Low % 2 === 0) {
+  for (i=Low+1 ; i<=Up ; i+=2){
+  Sum= i + Sum
+  }
+}
+Sum= Sum.toLocaleString("en");
+console.log("\n" + Sum + ".\n");
