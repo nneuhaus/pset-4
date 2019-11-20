65c1c6b150e2f59c7c19db8c7f7b246558f1a1d7
Nathalie Neuhaus <nneuhaus@ucvts.org>
2019-11-20 13:15:46 -0500

    really trying hard

---
+++ src/prime.js
@@ -0,0 +1,17 @@
+const readlineSync = require ("readline-sync");
+const MAX = 9007199254740991;
+const MIN = 1
+
+let Integer = Number(readlineSync.question("\nNon-negative number: "));
+let Result = 0;
+
+for (let x = 2; x <= Integer/2; x++){
+  Integer = Integer % x;
+  if(Integer == 0){
+    console.log("\nNot Prime.");
+    Result = 1;
+  }
+}
+if (Result == 0){
+  console.log("\nPrime.");
+}
